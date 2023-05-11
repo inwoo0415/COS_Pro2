3430ee3269b7b7c7a9c862bbda541bcc3369ec6c
inwoo0415 <118427831+inwoo0415@users.noreply.github.com>
2023-05-11 13:53:50 +0900

    Add files via upload

---
+++ 20230511/20230511_3.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int solution(int teakwondo, int running, int shooting[], int shooting_len) {
+	int answer = 0;
+
+	if (teakwondo >= 25)
+	{
+		answer += 250;
+	}
+	else
+	{
+		answer += teakwondo * 8;
+	}
+
+	answer += (running - 60) * (-5) + 250;
+
+	int count = 0;
+
+	for (int i = 0; i < shooting_len; i++) 
+	{
+		if (shooting[i] == 10) count++;
+		answer += shooting[i];
+	}
+	if (count >= 7) answer += 100;
+
+	return answer;
+}
+
+int main() {
+	int teakwondo = 27;
+	int running = 63;
+	int shooting[] = { 9,10,8,10,10,10,7,10,10,10 };
+	int shooting_len = 10;
+	int ret = solution(teakwondo, running, shooting, shooting_len);
+	printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+
+	return 0;
+}
